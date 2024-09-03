d3495e8ea4733a56a5438b80a76cea83d074f5f5
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-09-03 12:05:57 +0200

    asfa

---
+++ Feladat0Szamok/Feladat0Szamok/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string szam = Console.ReadLine();
+
+StreamWriter fajl = new StreamWriter("adatok.txt");
+while (szam != "0")
+{
+    szam = Console.ReadLine();
+    
+    fajl.WriteLine(szam+ szam * szam"");
+    fajl.WriteLine();
+
+}
+
+fajl.Close();
