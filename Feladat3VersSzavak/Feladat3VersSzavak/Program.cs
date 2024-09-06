f83b19b1e14eb658321f745ffe166e59797489db
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-09-06 13:31:30 +0200

    4nap

---
+++ Feladat3VersSzavak/Feladat3VersSzavak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat3VersSzavak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dick=new Dictionary<string, int>();
+            
+            StreamReader szoveg = new StreamReader("szoveg.txt");
+
+            while (!szoveg.EndOfStream) 
+            {
+                
+                Console.WriteLine(szoveg.ReadLine());
+            }
+
+            szoveg.Close();
+        }
+        
+    }
+}
