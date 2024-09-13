f1e4cec9307017446025922dd03053cd78d448fa
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-09-13 14:18:23 +0200

    5

---
+++ Feladat5ObjektumOrientalt/Feladat5ObjektumOrientalt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat5ObjektumOrientalt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kutya k =new kutya();
+            k.ugat();
+            k.suly = 12;
+            Console.WriteLine(k.suly);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Feladat5ObjektumOrientalt/Feladat5ObjektumOrientalt/kutya.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat5ObjektumOrientalt
+{
+    internal class kutya
+    {
+        public string nev;
+        public void ugat()
+        {
+            Console.WriteLine("Vauuu");
+        }
+        private float _suly;
+
+        public float suly
+        {
+            get
+            {
+                return _suly;
+            }
+            set
+            {
+                _suly= Math.Abs(value);
+            }
+        }
+        public kutya(string nev)
+        {
+            this.nev = nev;
+            Console.WriteLine("Kutya létrejött");
+        }
+        public override string ToString() {
+
+        }
+    }
+}
