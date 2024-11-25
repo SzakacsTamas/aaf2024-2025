dd0742eb61c93fcdcc8f335a600b92dfdc68a7ad
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-11-25 14:16:10 +0100

    fsdfsd

---
+++ Feladat11Vonat/Feladat11Vonat/Program.cs
@@ -0,0 +1,8 @@
+﻿using Feladat11Vonat;
+
+vonat k1 = new vonat(1);
+vonat k2 = new vonat(5);
+vonat k3= new vonat(100);
+
+Console.WriteLine(k1+k2+k3);
+Console.WriteLine(k1 - k2);
---
+++ Feladat11Vonat/Feladat11Vonat/vonat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat11Vonat
+{
+    internal class vonat
+    {
+        public int kocsi;
+
+        public vonat (int kocsi)
+        {
+            this.kocsi = kocsi;
+        }
+        public static vonat operator +(vonat v1,vonat v2)
+        {
+            return new vonat(v1.kocsi + v2.kocsi);
+        }
+        public static vonat operator -(vonat v1, vonat v2)
+        {
+            return new vonat(v2.kocsi - v1.kocsi);
+        }
+        public override string ToString()
+        {
+            return kocsi+" darab";
+        }
+    }
+}
