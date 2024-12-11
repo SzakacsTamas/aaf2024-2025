ec43bd7bd44623c8c5207ec4576f7473a258b202
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2024-12-11 13:27:52 +0100

    ghdfhgfhfg

---
+++ Feladat12ClassroomGyakorlás/Feladat12ClassroomGyakorlás/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+string text = File.ReadAllText("generated_log_2024-12-11T11_21_39.997Z.txt");
+
+
+Regex minta = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+
+
+var talalat = minta.Matches(text);
+Console.WriteLine(talalat.Count);
