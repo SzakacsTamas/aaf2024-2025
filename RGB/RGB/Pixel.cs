74d0b3384f9d7d8eb47bb83043a21fd416b5fb19
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2025-02-10 14:19:16 +0100

    ggsgdf

---
+++ RGB/RGB/Pixel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB
+{
+    internal class Pixel
+    {
+        public int x;
+        public int y;
+        public Szin rgb;
+
+        public Pixel(int x, int y, Szin rgb)
+        {
+            this.x = x;
+            this.y = y;
+            this.rgb = rgb;
+        }
+
+    }
+}
---
+++ RGB/RGB/Szin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB
+{
+    internal class Szin
+    {
+        public byte r;
+        public byte g; 
+        public byte b;
+        
+        public Szin(int red, int green, int blue)
+        {
+            r = (byte)red;
+            g = (byte)green;
+            b = (byte)blue;
+        }
+    }
+}
