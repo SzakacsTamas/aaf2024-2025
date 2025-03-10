3d4242be1e90328c2e7d7025a38a93e69bd4f5ea
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2025-03-10 14:04:17 +0100

    gsdg

--- kozosGyak/kozosGyak/MainWindow.xaml.cs
+++ kozosGyak/kozosGyak/MainWindow.xaml.cs
@@ -28,27 +28,67 @@ namespace kozosGyak
         {
             Button[,] gombok = new Button[sor, oszlop];
             grid.Children.Remove(startGomb);
-            for(int i=0; i<oszlop; i++)
+            for (int i = 0; i < oszlop; i++)
             {
-                for(int k = 0; k < sor; k++)
+                for (int k = 0; k < sor; k++)
                 {
                     Button g = new Button();
                     g.VerticalAlignment = VerticalAlignment.Top;
                     g.HorizontalAlignment = HorizontalAlignment.Left;
                     g.Height = 25;
                     g.Width = 25;
-                    
-                    g.Margin = new Thickness(i*25,k*25,0,0);
+
+                    g.Margin = new Thickness(i * 25, k * 25, 0, 0);
                     g.Click += kattinats;
                     grid.Children.Add(g);
                 }
 
             }
 
-            this.SizeToContent=SizeToContent.WidthAndHeight;
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+
             aknaLerak();
+            aknaMutat();
         }
+
+        int[,] aknaHely;
+        Random rand = new Random();
         void aknaLerak()
+        {
+            aknaHely = new int[sor, oszlop];
+
+
+            for (int k = 0; k < aknaDB; k++)
+            {
+                int randomSor = rand.Next(sor);
+                int randomOszlop = rand.Next(oszlop);
+                if (aknaHely[randomSor, randomOszlop] == 10)
+                {
+                    k--;
+                }
+                else
+                {
+                    aknaHely[randomSor, randomOszlop] = 10;//ezegydarabakna
+                }
+            }
+
+
+
+        }
+
+        void aknaMutat()
+        {
+            for (int k = 0; k < aknaHely.GetLength(0); k++)
+            {
+                for (int j = 0; j < aknaHely.GetLength(1); j++)
+                {
+                    gombok[k, j].Content = aknaHely[k, j];
+                }
+
+            }
+        }
+
+        int aknaSzamol(int sor, int oszlop)
         {
 
         }
--- kozosGyak/kozosGyak/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ kozosGyak/kozosGyak/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace kozosGyak {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/kozosGyak;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/kozosGyak;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
