c6da87cea359dadc19d41b0a55324dfe6b45ca57
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2025-04-10 08:45:04 +0200

    afsasdf

---
+++ NyelvvizsgaKonzolApp/NyelvvizsgaKonzolApp/EvAdat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyelvvizsgaKonzolApp
+{
+    internal class EvAdat
+    {
+        public int ev;
+        public int vizsgaDarab;
+        public EvAdat(int ev, int darab) 
+        {
+            this.ev = ev;
+            this.vizsgaDarab = darab;
+        }
+    }
+}
---
+++ NyelvvizsgaKonzolApp/NyelvvizsgaKonzolApp/NyelvAdat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyelvvizsgaKonzolApp
+{
+    internal class NyelvAdat
+    {
+        public string nyelv;
+        public List<EvAdat> vizsgak = new List<EvAdat>(); 
+        public NyelvAdat(string sor, int[] evszamok) 
+        {
+            string[] vag=sor.Split(';');
+            nyelv=vag[0];
+        }
+        public NyelvAdat(string nyelv, int[] adatok, int[] evszamok)
+        {
+            this.nyelv = nyelv;
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                this.vizsgak.Add(new EvAdat(evszamok[i],adatok[i]));
+            }
+        }
+    }
+}
