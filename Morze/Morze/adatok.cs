6da6419a3f7351dd9ac54ec70a8abbde2b08d462
Szakács Tamás <szakacst21@ady-nagyatad.sulinet.hu>
2025-05-20 13:15:36 +0200

    fdhf

---
+++ Morze/Morze/adatok.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morze
+{
+    internal class adatok
+    {
+        public string betu;
+        public string kod;
+
+        public adatok(string betu, string kod)
+        {
+            this.betu = betu;
+            this.kod = kod;
+        }
+    }
+}
---
+++ Morze/Morze/adatok2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morze
+{
+    internal class adatok2
+    {
+        public string szerzo;
+        public string idezet;
+
+        public adatok2(string szerzo, string idezet)
+        {
+            this.szerzo = szerzo;
+            this.idezet = idezet;
+        }
+    }
+}
